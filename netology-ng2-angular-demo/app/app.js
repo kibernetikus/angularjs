a0a4866cfcbf71a84b7188b27624a84f9ab682f8
Александр Поздняков <alexpozdnyakof@192.168.1.37.ipl.orionnet.ru>
2016-11-21 17:45:51 +0700

    homework lesson 1
    homework

---
+++ netology-ng2-angular-demo/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.when('/404', {
+            templateUrl: '404/404.html',
+            controller: 'View1Ctrl'
+        }).otherwise({redirectTo: '/404'});
+}]);
